b910fcca6a09b11a7b03ff0d6d4cfe1b3eb4c58e
Melissa Perez <melissaperezg.96@gmail.com>
2020-03-29 19:48:48 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+class Song 
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+   @@genres.uniq
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+ def self.genre_count
+   g_count = {}
+   @@genres.each do |el|
+     if genre_count|el|
+       g_count|el| += 1
+     else 
+       g_count|el| = 1
+     end
+   end
+   g_count
+ end
+
+   
+    attr_accessor :name, :artist, :genre 
+  
+  def initialize (name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre 
+    @@artists << artist
+  end
+  
+  
+  
+end
